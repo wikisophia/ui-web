d5c7fc6e51267111a4b4ec0ca0943eaa515e3284
Dave <knight-of-zero@users.noreply.github.com>
2018-07-01 03:45:19 -0700

    Made an API call to the arguments service.

--- packages/server/src/handlers/argument.ts
+++ packages/server/src/handlers/argument.ts
@@ -1,5 +1,6 @@
 import {apiAuthority, resourcesRoot} from '../config';
 import {Request, Response} from 'express';
+import axios, { AxiosResponse } from 'axios';
 
 const numRegex = /^[1-9]\d*$/
 
@@ -11,15 +12,38 @@ function handler(req: Request, res: Response): void {
     res.send(`argument with id=${req.params.id} does not exist`);
     return;
   }
-  res.render('argument', {
-    resourcesRoot,
-    apiAuthority,
-    id,
-    argument: {
-      conclusion: 'Socrates is mortal',
-      premises: ['All men are mortal', 'Socrates is a man']
+
+  axios.get(`http://${apiAuthority}/argument/${id}`, {
+    validateStatus: (status) => {
+      return (status >= 200 && status < 300) || (status >= 400 && status < 500)
     }
+  }).
+  then(apiResponseHandler(id, res)).
+  catch((err) => {
+    res.status(503);
+    res.send(`Failed call to arguments service: ${err}`);
   });
+}
+
+function apiResponseHandler(id: Number, uiResponse: Response) {
+  return (apiResponse: AxiosResponse<any>) => {
+    if (apiResponse.status >= 200 && apiResponse.status < 300) {
+      uiResponse.render('argument', {
+        resourcesRoot,
+        apiAuthority,
+        id,
+        argument: apiResponse.data,
+      });
+    }
+    else if (apiResponse.status === 404) {
+      uiResponse.status(apiResponse.status);
+      uiResponse.send(`Argument with id=${id} does not exist.`);
+    }
+    else if (apiResponse.status >= 400 && apiResponse.status < 500) {
+      uiResponse.status(apiResponse.status);
+      uiResponse.send(apiResponse.data);
+    }
+  }
 }
 
 export default handler;
