493c23f82bc9a791b19c803cb43b41dea9a35198
Dave <knight-of-zero@users.noreply.github.com>
2018-07-01 01:55:50 -0700

    more integration fixes.

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,4 +1,4 @@
 // This module defines the environment variables used throughout the app.
 
 export const apiAuthority = process.env.WKSPH_UI_API_AUTHORITY || 'localhost:8001';
-export const resourcesRoot = process.env.WKSPH_UI_RESOURCES_ROOT || `http://localhost:8080`;
+export const resourcesRoot = process.env.WKSPH_UI_RESOURCES_ROOT || `http://localhost:4041`;
