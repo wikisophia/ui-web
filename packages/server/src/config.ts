4099a44e70992df05685711d282506a1d536ddb7
Dave <knight-of-zero@users.noreply.github.com>
2019-02-26 18:03:59 -0500

    Updated all the dependencies.

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,7 +1,7 @@
 // This module defines the environment variables used throughout the app.
 
 export const apiAuthority = process.env.WKSPH_UI_API_AUTHORITY || 'localhost:8001';
-export const resourcesRoot = process.env.WKSPH_UI_RESOURCES_ROOT || `http://localhost:4041`;
+export const resourcesRoot = process.env.WKSPH_UI_RESOURCES_ROOT || 'http://localhost:4041';
 
 let port = 4040;
 if (process.env.WKSPH_UI_SERVER_PORT) {
